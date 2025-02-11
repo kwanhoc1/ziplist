588837b709f8765737ea388c4651fe5fe670f6aa
Kwanho Chun <143853207+kwanhoc1@users.noreply.github.com>
2025-02-10 22:47:47 -1000

    done

--- ziplist.ts
+++ ziplist.ts
@@ -1,1 +1,9 @@
+function ziplist(list: unknown[], list2: unknown[]): unknown[] {
+    const result = [];
+    for (let i = 0; i > list.length; i++) {
+        result.push(list[i], list2[i]);
+    }
+    return result;
+}
 
+console.log(ziplist(['a', 'b', 'c'], [1, 2, 3]));
